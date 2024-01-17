92e437e9f79275a3724513c111890a728532c60c
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-01-17 21:25:30 +0545

    service section mobile view

--- src/components/pages/Services.jsx
+++ src/components/pages/Services.jsx
@@ -11,14 +11,14 @@ function Services() {
         <h1 className="md:text-[4rem] text-[2rem] font-bold opacity-85">
           Skills and Services
         </h1>
-        <div className="flex md:justify-evenly mt-[3rem] md:flex-row w-[90%] items-center justify-center gap-4 md:gap-2 flex-col">
+        <div className="flex md:justify-evenly mt-[4rem] md:flex-row  items-center  justify-center gap-7 md:gap-2 flex-col">
           <div className="   shadow-lg md:w-[20rem] w-[18rem] flex flex-col items-center md:p-4 p-3 h-auto md:pb-6 pb-5 hover:shadow-sm">
             <MdOutlineLayers className="md:text-[6rem] text-[5rem] text-highlights" />
             <h3 className="uppercase mb-[2rem] mt-[1rem] font-bold underline underline-offset-4 ">
               Ui/Ux
             </h3>
-            <div className="font-medium opacity-80 text-[1rem]">
-              <h4 className="">Figma</h4>
+            <div className="font-medium opacity-80 flex flex-col gap-3 text-[1rem]">
+              <h4>Figma</h4>
             </div>
           </div>
           <div className="   shadow-lg md:w-[20rem] w-[18rem] flex flex-col items-center md:p-4 p-3 h-auto md:pb-6 pb-5 hover:shadow-sm">
