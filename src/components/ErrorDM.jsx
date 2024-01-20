9bce3bd8a79100d45dfb60443538efdfe2c6cf22
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-01-20 14:12:47 +0545

    time added for message box

--- src/components/ErrorDM.jsx
+++ src/components/ErrorDM.jsx
@@ -1,43 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 
-// eslint-disable-next-line react/prop-types
 function ErrorDM() {
   const [showMsg, setShowMsg] = useState(false);
   const [loading, setLoading] = useState(false);
   const [hasScrollDown, setHasScrollDown] = useState(false);
+  const timeoutRef = useRef(null);
 
-  function handleMessage() {
+  const handleMessage = useCallback(() => {
     if (window.scrollY > 200 && !hasScrollDown) {
       setShowMsg(true);
       setLoading(true);
-      setHasScrollDown(true); // Set the flag to true after the first scroll down
+      setHasScrollDown(true);
     }
-  }
+  }, [hasScrollDown]);
 
   useEffect(() => {
     window.addEventListener("scroll", handleMessage);
 
     return () => {
       window.removeEventListener("scroll", handleMessage);
+      clearTimeout(timeoutRef.current);
     };
-  }, [hasScrollDown]);
+  }, [handleMessage, hasScrollDown]);
 
   useEffect(() => {
     if (showMsg) {
-      const timeoutId = setTimeout(() => {
+      timeoutRef.current = setTimeout(() => {
         setShowMsg(false);
         setLoading(false);
-      }, 8000);
-
-      return () => clearTimeout(timeoutId);
+      }, 10000);
     }
+
+    return () => clearTimeout(timeoutRef.current);
   }, [showMsg]);
+
   return (
     <div>
       {showMsg && (
         <div className={`error-message ${showMsg ? "show" : ""}`}>
           <h1 className="md:p-4 p-2">
-            Darkmode is under maintainance in these sections.
+            Darkmode is under maintenance in these sections.
             {loading && <div className="loading-bar"></div>}
           </h1>
         </div>
