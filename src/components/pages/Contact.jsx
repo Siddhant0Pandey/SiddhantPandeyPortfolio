ebe6991f49543fba487357a88fd9abf4cf0bd9d3
Siddhant0Pandey <pandeysiddhant0922@gmail.com>
2024-01-20 21:42:08 +0545

    contact button hover applied

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -112,7 +112,7 @@ function Contact() {
             </div>
             <button
               type="submit"
-              className="bg-highlights text-white px-4 py-2 rounded-md"
+              className="bg-highlights text-white px-4 py-2 rounded-md hover:opacity-85"
             >
               Submit
             </button>
